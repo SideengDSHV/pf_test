e85aef2b33b48022694aa84f8fc7534560cc8a61
SideengDSHV <sideengdshv@yandex.ru>
2024-02-05 06:33:04 +0600

    CharacterController script

--- Assets/Gameplay/Characters/Scripts/CharacterController.cs
+++ Assets/Gameplay/Characters/Scripts/CharacterController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class HumanCharacterController : MonoBehaviour
+namespace Gameplay.Characters
 {
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(NavMeshAgent))] [RequireComponent(typeof(Animator))]
+    public class HumanCharacterController : MonoBehaviour
     {
-        
-    }
+        private static readonly int IsRunning = Animator.StringToHash("isRunning");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
+        private Camera _camera;
+
+        [SerializeField] private float animationStoppingDistance;
+
+        private void OnEnable()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1")) SetDestinationToMouse();
+
+            SyncAnimator();
+        }
+
+        private void SetDestinationToMouse()
+        {
+            if (!Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit)) return;
+
+            // Check if target is too close
+            if (Vector3.Distance(hit.point, transform.position) < animationStoppingDistance) return;
+
+            NavMeshPath potentialPath = new();
+            _navMeshAgent.CalculatePath(hit.point, potentialPath);
+
+            // Check if path is reachable
+            if (_navMeshAgent.pathStatus != NavMeshPathStatus.PathComplete) return;
+
+            _navMeshAgent.SetPath(potentialPath);
+        }
+
+        private void SyncAnimator()
+        {
+            bool _isRunning = _navMeshAgent.hasPath && _navMeshAgent.remainingDistance > animationStoppingDistance;
+            _animator.SetBool(IsRunning, _isRunning);
+        }
     }
 }
